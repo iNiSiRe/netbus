46826e34e41b11749ad9953817612ad4f4d1ade7
inisire <inisire@gmail.com>
2023-07-22 22:43:13 +0300

    eventbus: subscribe

--- src/Event/EventBusInterface.php
+++ src/Event/EventBusInterface.php
@@ -6,7 +6,9 @@ interface EventBusInterface
 {
     public function createSource(string $source): EventSourceInterface;
 
-    public function subscribe(EventSubscriber $subscriber): void;
+    public function registerSubscriber(EventSubscriber $subscriber): void;
+
+    public function subscribe(SubscriptionInterface $subscription): void;
 
     public function dispatch(string $source, EventInterface $event): void;
 }
