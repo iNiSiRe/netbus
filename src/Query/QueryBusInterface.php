54a6643746f36c927ce83bed12891c9cdc3f9baa
inisire <inisire@gmail.com>
2023-07-26 01:16:44 +0300

    query bus

--- src/Query/QueryBusInterface.php
+++ src/Query/QueryBusInterface.php
@@ -5,8 +5,4 @@ namespace inisire\NetBus\Query;
 interface QueryBusInterface
 {
     public function execute(string $destinationId, string $name, array $data = []): ResultInterface;
-
-    public function registerHandler(string $busId, QueryHandlerInterface $handler): void;
-
-    public function on(Route $route, callable $handler): void;
 }
